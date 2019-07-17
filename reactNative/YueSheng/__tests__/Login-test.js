578c0f825762ed00b885ec19499abaa5973cca58
xiaxiao <2625666138@qq.com>
2019-07-17 13:49:41 +0800

    add login & register Test

---
+++ reactNative/YueSheng/__tests__/Login-test.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import LoginScreen from '../src/pages/login/LoginScreen';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+    const tree = renderer.create(<LoginScreen />);
+    expect(tree).toMatchSnapshot();
+});
